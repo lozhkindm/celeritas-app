66906b5bca3fb4f5c7043aad4c2ea51f1ca7eeb1
Dmitriy <lozhkindm@yandex.ru>
2022-03-27 09:42:06 +0300

    aliases for routes

--- routes.go
+++ routes.go
@@ -14,17 +14,17 @@ func (a *application) routes() *chi.Mux {
 	// middlewares
 
 	// routes
-	a.App.Routes.Get("/", a.Handlers.Home)
-	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
-	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
-	a.App.Routes.Get("/sessions", a.Handlers.Sessions)
-	a.App.Routes.Get("/users/login", a.Handlers.UserLogin)
-	a.App.Routes.Post("/users/login", a.Handlers.PostUserLogin)
-	a.App.Routes.Get("/users/logout", a.Handlers.UserLogout)
-	a.App.Routes.Get("/form", a.Handlers.ShowForm)
-	a.App.Routes.Post("/form", a.Handlers.SubmitForm)
+	a.routeGet("/", a.Handlers.Home)
+	a.routeGet("/go-page", a.Handlers.GoPage)
+	a.routeGet("/jet-page", a.Handlers.JetPage)
+	a.routeGet("/sessions", a.Handlers.Sessions)
+	a.routeGet("/users/login", a.Handlers.UserLogin)
+	a.routePost("/users/login", a.Handlers.PostUserLogin)
+	a.routeGet("/users/logout", a.Handlers.UserLogout)
+	a.routeGet("/form", a.Handlers.ShowForm)
+	a.routePost("/form", a.Handlers.SubmitForm)
 
-	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
+	a.routeGet("/create-user", func(w http.ResponseWriter, r *http.Request) {
 		usr := data.User{
 			FirstName: a.App.RandStr(10),
 			LastName:  a.App.RandStr(10),
@@ -39,7 +39,7 @@ func (a *application) routes() *chi.Mux {
 		}
 		fmt.Fprintf(w, "%d: %s", id, usr.FirstName)
 	})
-	a.App.Routes.Get("/get-all-users", func(w http.ResponseWriter, r *http.Request) {
+	a.routeGet("/get-all-users", func(w http.ResponseWriter, r *http.Request) {
 		users, err := a.Models.Users.GetAll()
 		if err != nil {
 			a.App.ErrorLog.Println(err)
@@ -49,7 +49,7 @@ func (a *application) routes() *chi.Mux {
 			fmt.Fprintf(w, "%+v", u)
 		}
 	})
-	a.App.Routes.Get("/get-user/{id}", func(w http.ResponseWriter, r *http.Request) {
+	a.routeGet("/get-user/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
 		user, err := a.Models.Users.GetById(id)
 		if err != nil {
@@ -58,7 +58,7 @@ func (a *application) routes() *chi.Mux {
 		}
 		fmt.Fprintf(w, "%+v", user)
 	})
-	a.App.Routes.Get("/update-user/{id}", func(w http.ResponseWriter, r *http.Request) {
+	a.routeGet("/update-user/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
 		user, err := a.Models.Users.GetById(id)
 		if err != nil {
