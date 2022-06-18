e32e19920ff57d3910eaf4d3fdd6397db821ef72
Dmitriy <lozhkindm@yandex.ru>
2022-06-18 20:47:43 +0300

    simple handler test

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
@@ -16,5 +17,26 @@ func TestHandlers_Home(t *testing.T) {
 
 	if res.StatusCode != 200 {
 		t.Errorf("expected status 200, bot %d", res.StatusCode)
+	}
+}
+
+func TestHandlers_Home2(t *testing.T) {
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Errorf("failed to create a request: %s", err)
+	}
+
+	r = r.WithContext(getCtx(r))
+	w := httptest.NewRecorder()
+
+	cel.Session.Put(r.Context(), "test_key", "Hello")
+	h := http.HandlerFunc(testHandlers.Home)
+	h.ServeHTTP(w, r)
+
+	if w.Code != 200 {
+		t.Errorf("expected status 200, bot %d", w.Code)
+	}
+	if cel.Session.GetString(r.Context(), "test_key") != "Hello" {
+		t.Error("got wrong value from session")
 	}
 }
