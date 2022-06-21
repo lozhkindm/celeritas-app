bfe17b4b833ad9c9226b819bfd797698a4fa160e
Dmitriy <lozhkindm@yandex.ru>
2022-06-21 08:52:42 +0300

    test screenshots

--- handlers/setup_test.go
+++ handlers/setup_test.go
@@ -70,6 +70,7 @@ func getRoutes() http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(cel.SessionLoad)
 	mux.Get("/", testHandlers.Home)
+	mux.Handle("/public/*", http.StripPrefix("/public", http.FileServer(http.Dir("./../public"))))
 	return mux
 }
 
